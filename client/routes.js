8a4522d746857831cd4c8d020c6a14d342dcd082
ohatakky <ohatakky@gmail.com>
2019-03-03 16:55:07 +0900

    admin機能をclientから削除

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Home from './components/home'
-import Admin from './components/admin'
 import Board from './components/board'
 
 const Routes = () => (
@@ -10,8 +9,6 @@ const Routes = () => (
         <div>
           <Switch>
             <Route exact path='/' component={Home} />
-            {/* <Route path='/admin/create' component={Admin} /> */}
-            <Route path='/admin' component={Admin} />
             <Route path='/boards/:hash' component={Board} />
             <Route path='/boards' component={Board} />
           </Switch>
