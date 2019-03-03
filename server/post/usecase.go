e815afa477959f3681135681ff763fd9b605a30e
ohatakky <ohatakky@gmail.com>
2019-03-03 15:16:00 +0900

    postのstore処理実装

--- server/post/usecase.go
+++ server/post/usecase.go
@@ -3,6 +3,5 @@ package post
 import "questions-board/server/models"
 
 type Usecase interface {
-	Get(*models.Board) ([]*models.Post, error)
 	Store(*models.Post) error
 }
---
+++ server/post/usecase/post.go
@@ -0,0 +1,36 @@
+package usecase
+
+import (
+	"log"
+	"questions-board/server/board"
+	"questions-board/server/models"
+	"questions-board/server/post"
+)
+
+type postUsecase struct {
+	boardRepo board.Repository
+	postRepo  post.Repository
+}
+
+func NewPostUsecase(pr post.Repository) post.Usecase {
+	return &postUsecase{
+		postRepo: pr,
+	}
+}
+
+func (pu *postUsecase) Store(p *models.Post) error {
+
+	url := p.Board.Url
+	_, err1 := pu.boardRepo.Get(url)
+	if err1 != nil {
+		log.Fatal(err1)
+	}
+
+	// ボードへのアクセスが正しければstore
+	err2 := pu.postRepo.Store(p)
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	return nil
+}
