0498dea3d7fc473e9899dea4ccf412666ba8cc64
ohatakky <ohatakky@gmail.com>
2019-03-03 18:56:30 +0900

    clientからserverをcallする

--- client/components/board/index.js
+++ client/components/board/index.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 
 class Board extends Component {
+  constructor(props) {
+    // const hash = this.props.match.params.hash;
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:1234/boards/basvabjv")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
   render() {
-    const hash = this.props.match.params.hash
-    return (
-      <h1>Boards {hash}</h1>
-    );
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          {items[0].content}
+        </div>
+      );
+    }
   }
 }
 
