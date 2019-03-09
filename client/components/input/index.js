74cb55a412fabddbaf0ce2f8fe4bcad1a920affa
ohatakky <ohatakky@gmail.com>
2019-03-09 19:51:25 +0900

    TODO追加

--- client/components/input/index.js
+++ client/components/input/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 class Input extends Component {
 
+  // TODO : input form 追加
+
+  // TODO : post API 叩く
+
     render() {
         return (
           <div>
