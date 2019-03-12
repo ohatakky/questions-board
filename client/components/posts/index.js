730ea4dd14079a87508fda499c1368680566266b
ohatakky <ohatakky@gmail.com>
2019-03-13 04:17:06 +0900

    TODO追加

--- client/components/posts/index.js
+++ client/components/posts/index.js
@@ -3,6 +3,7 @@ import Post from './post'
 
 function Posts(props) {
     
+    // TODO : 下から上に更新されるようにしたい
     const posts = props.posts.map((post) =>
         <Post post={post} key={post.id} />
     );
