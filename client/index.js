2fe14eb98696f03fe150686654697187b8a831e9
ohatakky <ohatakky@gmail.com>
2019-03-23 23:49:23 +0900

    AppBarを追加

--- client/index.js
+++ client/index.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes'
+import Routes from './routes';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import * as colors from '@material-ui/core/colors';
 
 ReactDOM.render(
-  <Routes />,
+  <React.Fragment>
+    <CssBaseline />
+    <AppBar position="static" style={{backgroundColor: colors.lightBlue[400]}}>
+      <Toolbar>
+        <Typography variant="title" style={{color: colors.grey[50]}}>
+          Questions-Board
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Routes />
+  </React.Fragment>,
   document.getElementById('app')
 );
