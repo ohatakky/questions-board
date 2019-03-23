2ff5371a5a41e6a8fde1264dd8f670f4ebf58367
ohatakky <ohatakky@gmail.com>
2019-03-23 19:01:36 +0900

    降順に表示

--- client/components/posts/index.js
+++ client/components/posts/index.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import Post from './post'
 
 function Posts(props) {
-    
-    // TODO : 下から上に更新されるようにしたい
     const posts = props.posts.map((post) =>
         <Post post={post} key={post.id} />
     );
 
+    const rev_posts = posts.slice().reverse();
+
     return (
         <div>
-            {posts}
+            {rev_posts}
         </div>
     );
 }
